4d27e3dd816aa65e0b8f32e3d6035feca83af070
Jaakko Ruohio <jaakkor2@gmail.com>
2024-01-14 23:28:07 +0200

    Functionality and tests for the example1.jmp pass

--- src/JMPReader.jl
+++ src/JMPReader.jl
@@ -1,5 +1,177 @@
 module JMPReader
 
-greet() = print("Hello World!")
+export readjmp
+
+using Dates: unix2datetime, Date, DateTime
+using DataFrames: DataFrame
+
+struct Column
+    names::Vector{String}
+    datatypes::Vector{Int16}
+    offsets::Vector{Int64}
+end
+
+struct Info
+    savetime::DateTime
+    nrows::Int
+    ncols::Int
+    column::Column
+end
+
+
+
+"""
+    function readjmp(fn::AbstractString)
+
+Read a JMP file.
+"""
+function readjmp(fn::AbstractString)
+    isfile(fn) || (@error("File `$fn` does not exist`"); return nothing)
+    a = read(fn)
+    len = length(a)
+    len < 507 && (@error("File `$fn` length=$len.  Maybe truncated?"); return nothing)
+    nrows = reinterpret(Int64, a[368 .+ (1:8)])[1]
+    ncols = reinterpret(Int32, a[376 .+ (1:4)])[1]
+    savetime = to_datetime(a[407:414])[1]
+    unknown1 = reinterpret(Int16, a[482:483])[1]  ## unknown parameter causing different offsets in columns data
+    unknown2 = reinterpret(Int32, a[484:487])[1]
+    unknown3 = reinterpret(Int16, a[492:493])[1]
+    unknown4 = reinterpret(Int16, a[494:495])[1]  ## unknown parameter causing different offsets in columns data
+    offset_colinfo = 503
+    if unknown1 == 4
+        offset_colinfo += 10 + unknown4
+    end
+    unknown1 in [3,4] || @warn("Never before seen unknown1=$unknown1")
+    
+    ncols2 = reinterpret(Int32, a[offset_colinfo .+ (1:4)])[1]
+    unknown5 = reinterpret(Int16, a[offset_colinfo + 14 .+ (1:2)])[1]
+    
+    colidxlist = reinterpret(Int32, a[offset_colinfo + 16 .+ (1:4*ncols)])
+    colidxlist == 0:ncols-1 || @warn("`offset_colinfo`-variable probably has a wrong value")
+    coltypes = reinterpret(Int16, a[offset_colinfo + 16 + 4*ncols .+ (1:2*ncols)])
+    ###    @show a[offset_colinfo + 16 + 6*ncols+1 : offset_colinfo + 62 + 6*ncols-1]  # possible unknown bytes
+    unknowns6 = Int.(a[offset_colinfo + 16 + 6*ncols .+ [31,39,41,43,47]])  # 47 is often equal to ncols, but not always
+    @show unknown1, unknown2, unknown3, unknown4, unknown5, unknowns6
+    coldata_offset = offset_colinfo + 16 + 6*ncols # 1-9
+
+    ncols == ncols2 || @error("Number of columns from two different locations do not match, $ncols vs $ncols2")
+
+    colnames, coloffsets = column_info(a, coldata_offset, ncols)
+    
+    info = Info(savetime, nrows, ncols, Column(colnames, coltypes, coloffsets))
+    alldata = [column_data(a, info, i) for i in 1:info.ncols]
+    return DataFrame(alldata, info.column.names)
+end
+
+function column_rawdata(data, info, i::Int)
+    1 ≤ i ≤ info.ncols || error("requested column $i is out-of-bounds")
+    i == info.ncols && return data[info.column.offsets[i]+1:end]
+    return data[info.column.offsets[i]+1:info.column.offsets[i+1]]
+end
+
+"""
+    column_data(data, info, i::Int)
+
+Return data from `i`th column.
+"""
+function column_data(data, info, i::Int)
+    1 ≤ i ≤ info.ncols || error("requested column $i is out-of-bounds")
+    raw = column_rawdata(data, info, i)
+    dtype = info.column.datatypes[i]
+
+    # Float64
+    if dtype in [38,49,53,60,62,67,78,155]
+        out = reinterpret(Float64, raw[end-8*info.nrows+1:end])
+        out = replace(out, NaN => missing)
+        return out
+    end
+
+    # Date
+    if dtype in [61]
+        out = to_datetime(raw[end-8*info.nrows+1:end])
+        return [ismissing(x) ? missing : Date(x) for x in out]
+    end
+
+    # Time
+    if dtype in [102,108]
+        return to_datetime(raw[end-8*info.nrows+1:end])
+    end
+
+    # Duration
+    if dtype in [73]
+        return to_datetime(raw[end-8*info.nrows+1:end]) .- DateTime(1904,1,1,0,0,0)
+    end
+
+    # Character, const width < 127
+    # 44 is formula, now return the strings
+    if dtype in [54,93,123,169,44]
+        width = raw[raw[1] + 7]
+        io = IOBuffer(raw[end-info.nrows*width+1:end])
+        str = [String(read(io, width)) for i in 1:info.nrows]
+        str = rstrip.(str, '\0')
+        str = String.(str) # SubString->String
+        return str
+    end
+
+    # Character, variable width
+    if dtype in [50,76,169,360]
+        ofs = raw[1] + raw[raw[1]+25] # offset to width data
+        if dtype in [50,76,169] # Int8
+            widths = reinterpret(Int8, raw[ofs + 41 .+ (1:info.nrows)])
+        else # Int16
+            widths = reinterpret(Int16, raw[ofs + 41 .+ (1:2*info.nrows)])
+        end
+        io = IOBuffer(raw[end-sum(widths)+1:end])
+        str = [String(read(io, widths[i])) for i in 1:info.nrows]
+        return str
+    end
+    
+    @error("Data type $dtype not implemented of column $(info.column.names[i]) (i=$i), returning raw data")
+    return raw
+end
+
+function column_data(data, info, name::Union{String,Regex})
+    name in info.column.names || error("Column name $name does not exist")
+    if isa(name, String)
+        fun = isequal(name)
+    else
+        fun = x -> !isnothing(match(name, x))
+    end
+    idx = findall(fun, info.column.names)
+    columns = [column_data(data, info, i) for i in idx]
+    names = info.column.names[idx]
+    return DataFrame(columns, names)
+end
+
+
+"""
+    column_info(data, offset, ncols)
+
+Return column names and offsets to column data.
+"""
+function column_info(data, offset, ncols)
+    hacky_offset = data[offset + 31] + 42
+    coloffsets = reinterpret(Int64, data[offset + hacky_offset .+ (1:8*ncols)])
+    colnames = String[]
+    for i in coloffsets
+        width = data[1+i]
+        data[2+i] == 0 || @info("Byte with previously unseen content $(data[2+i]) at $(2+i)")
+        push!(colnames, String(data[2+i .+ (1:width)]))
+    end
+    return colnames, coloffsets
+end
+
+"""
+    to_datetime(a::Vector{UInt8})
+
+Convert a 8-multiple vector of `UInt8`s to vector of `DateTime`s.
+JMP uses the 1904 date system, correct for that.
+"""
+function to_datetime(a::Vector{UInt8})
+    mod(length(a), 8) == 0 || error("Length should be a multiple of 8")
+    floats = reinterpret(Float64, a)
+    dt = [isnan(float) ? missing : unix2datetime(float) for float in floats]
+    dt .- (Date(1970, 1, 1) - Date(1904, 1, 1))
+end
 
 end # module JMPReader
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,22 @@
 using Test
 using JMPReader
+using Dates: Date, DateTime, Second
+
+@testset "example1.jmp" begin
+    df = readjmp(joinpath(@__DIR__, "example1.jmp"))
+    @test df.ints == [1.0,2.0,3.0,4.0]
+    @test df.floats == [11.1,22.2,33.3,44.4]
+    @test df.charconstwidth == ["a","b","c","d"]
+    @test df.time[[1,2,3]] == [DateTime(1976,4,1,21,12), DateTime(1984,8,6,23,58), DateTime(2003,6,2,17)]
+    @test ismissing(df.time[4])
+    @test df.date[[1,2,4]] == [Date(2024,1,13), Date(2024,1,14), Date(2032,2,12)]
+    @test ismissing(df.date[3])
+    @test df.duration == [Second(2322), Second(364), Second(229), Second(0)]
+    @test df.charconstwidth2 == ["a","bb","ccc","dddd"]
+    @test df.charvariable16 == ["aa","bbbb","cccccccc","abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij"]
+    @test df.formula == ["2","4","6","8"]
+    @test df.pressures[[1,3,4]] == [101.325,2.6,4.6e113]
+    @test ismissing(df.pressures[2])
+    @test df."char utf8" == ["ꙮꙮꙮ","🚴💨","jäääär","辛口"]
+    @test df.charvariable8 == ["a","bb","cc","abcdefghijkl"]
+end
